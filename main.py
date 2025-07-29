45f4764ce4bd21795c3d6ca366bc5c74685bc5c8
byungKHee <byungheekang@g.skku.edu>
2025-07-29 16:28:37 +0900

    fix: 함수 순서 변경

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from db import SessionLocal, engine, Base
 
 app = FastAPI()
 
-# 데이터베이스 연결 확인
-@app.get("/ping-db")
-def ping_db(db: Session = Depends(get_db)):
-    db.execute(text("SELECT 1"))
-    return {"status": "ok"}
-
 # 데이터베이스 세션 생성
 def get_db():
     db = SessionLocal()
@@ -19,6 +13,11 @@ def get_db():
     finally:
         db.close()
 
+# 데이터베이스 연결 확인
+@app.get("/ping-db")
+def ping_db(db: Session = Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {"status": "ok"}
 
 # 데이터베이스 테이블 생성
 # TODO: 추후 alembic 적용 시 삭제
